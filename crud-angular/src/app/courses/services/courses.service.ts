c2c85d58cc54f1e873f32df5e02a1a29227dab64
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-10-10 16:29:18 -0300

    CRUD Angular + Spring | 12 - Lista de Cursos: Pipe para mostrar ícone

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -10,7 +10,7 @@ import { Course } from '../model/course';
 })
 export class CoursesService {
 
-private readonly  API = '/assets/acourses.json';
+private readonly  API = '/assets/courses.json';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -18,7 +18,7 @@ private readonly  API = '/assets/acourses.json';
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      delay(5000),
+      // delay(5000),
       tap(courses => console.log(courses))
     );
   }
--- crud-angular/src/app/shared/shared.module.ts
+++ crud-angular/src/app/shared/shared.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { CategoryPipe } from './pipes/category.pipe';
 
 @NgModule({
-  declarations: [ErrorDialogComponent],
-  imports: [AppMaterialModule, CommonModule],
-  exports: [ErrorDialogComponent],
+  declarations: [
+    ErrorDialogComponent, 
+    CategoryPipe
+  ],
+  imports: [
+    AppMaterialModule, 
+    CommonModule
+  ],
+  exports: [
+    ErrorDialogComponent, 
+    CategoryPipe
+  ],
 })
 export class SharedModule {}
