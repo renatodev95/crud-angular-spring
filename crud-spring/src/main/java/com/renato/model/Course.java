f03ec9ef662dda31f048eb06a30e79c17d3c74ff
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-10-10 18:07:25 -0300

    CRUD Angular + Spring | 15: Listar Cursos (API HTTP GET)

---
+++ crud-spring/src/main/java/com/renato/model/Course.java
@@ -0,0 +1,21 @@
+package com.renato.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+//@Table(name = "cursos")
+public class Course {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false)
+    private String category;
+}
