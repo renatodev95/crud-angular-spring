a59184c3917441f02f070d68c4aea12bc27cc154
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-10-11 16:33:17 -0300

    CRUD Angular + Spring | 21: Formulário: API Spring: Criar Curso (HTTP POST)

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,7 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Course } from '../model/course';
 
 import { CoursesService } from '../services/courses.service';
 
@@ -17,7 +17,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -29,12 +30,19 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
-      (result) => console.log(result),
+      (result) => this.onSuccess(),
       (error) => this.onError()
     );
   }
 
-  onCancel() {}
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', {duration: 5000});
+    this.onCancel();
+  }
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso.', '', {duration: 5000});
--- crud-spring/src/main/java/com/renato/controller/CourseController.java
+++ crud-spring/src/main/java/com/renato/controller/CourseController.java
@@ -3,9 +3,9 @@ package com.renato.controller;
 import com.renato.model.Course;
 import com.renato.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +17,13 @@ public class CourseController {
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> listAll() {
+    public @ResponseBody List<Course> listAll() {
         return courseRepository.findAll();
+    }
+    
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
