216340d672a620598a57a638c4c17e94a1d07b63
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-12-21 14:53:12 -0300

    33: Dialog de Confirmação

--- crud-angular/src/app/shared/shared.module.ts
+++ crud-angular/src/app/shared/shared.module.ts
@@ -4,18 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { CategoryPipe } from './pipes/category.pipe';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 
 @NgModule({
   declarations: [
-    ErrorDialogComponent, 
-    CategoryPipe
+    ErrorDialogComponent,
+    CategoryPipe, ConfirmationDialogComponent
   ],
   imports: [
-    AppMaterialModule, 
+    AppMaterialModule,
     CommonModule
   ],
   exports: [
-    ErrorDialogComponent, 
+    ErrorDialogComponent,
+    ConfirmationDialogComponent,
     CategoryPipe
   ],
 })
