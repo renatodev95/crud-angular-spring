c54c838bbb204f7ea99c64ac252e09c9edec1c8c
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2023-01-02 14:18:13 -0300

    36: Remoção Lógica (Soft Delete)

--- crud-spring/src/main/java/com/renato/model/Course.java
+++ crud-spring/src/main/java/com/renato/model/Course.java
@@ -2,6 +2,8 @@ package com.renato.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -12,6 +14,8 @@ import javax.validation.constraints.Pattern;
 @Data
 @Entity
 //@Table(name = "cursos")
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
     
     @Id
@@ -26,8 +30,16 @@ public class Course {
     private String name;
 
     @NotNull
+    @Length(max = 10)
     @NotBlank
-    @Pattern(regexp = "Back-end | Front-end")
+    @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @NotBlank
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 }
