7f078f4ea36db1724a12da06ab20b01097b86ab1
renatodev95 <renatodev95@gmail.com>
2023-05-04 16:24:18 -0300

    CRUD Angular + Spring | 45: Curso-Aulas: OneToMany

--- crud-spring/src/main/java/com/renato/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/renato/CrudSpringApplication.java
@@ -2,6 +2,7 @@ package com.renato;
 
 import com.renato.enums.Category;
 import com.renato.model.Course;
+import com.renato.model.Lesson;
 import com.renato.repository.CourseRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,9 +20,17 @@ public class CrudSpringApplication {
 	CommandLineRunner initDatabase(CourseRepository courseRepository) {
 		return args -> {
 			courseRepository.deleteAll();
+
 			Course c = new Course();
 			c.setName("Angular com Spring");
 			c.setCategory(Category.BACK_END);
+
+			Lesson lesson = new Lesson();
+			lesson.setName("Introdução");
+			lesson.setYoutubeUrl("Nb4uxLxdvxo");
+			lesson.setCourse(c);
+			c.getLessons().add(lesson);
+
 			courseRepository.save(c);
 		};
 	}
